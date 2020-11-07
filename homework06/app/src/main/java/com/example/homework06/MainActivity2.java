1c67bcb23e30c742d9df7e6734fcec3cfb0407a9
xiaorui293 <desenyang@andsmind.com>
2020-11-07 09:57:04 +0800

    homework06

---
+++ homework06/app/src/main/java/com/example/homework06/MainActivity2.java
@@ -0,0 +1,53 @@
+package com.example.homework06;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(MainActivity2.this, TextWriteActivity.class);
+                startActivity(intent);
+
+            }
+        });
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(MainActivity2.this, TextReadActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(MainActivity2.this, ImageWriteActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button button4 = (Button) findViewById(R.id.button4);
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(MainActivity2.this, ImageReadActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
